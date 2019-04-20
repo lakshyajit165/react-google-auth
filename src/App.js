20cb5d82be075954978505cd142c8b2416f52494
Lakshyajit <lakshyajit@localhost.localdomain>
2019-04-20 22:55:31 +0530

    react authentication

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
 
 class App extends Component {
   render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle
+    } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br/>
+        <br/>
+        <div className="card col-lg-6 mx-auto">
+          <div className="card-body">
+            <h2>React Authentication App</h2>
+            <hr/>
+          {
+            user
+              ? 
+                <p className="lead">Hello, {user.displayName}</p>
+              : <p className="lead">Please sign in.</p>
+          }
+          {
+            user
+              ? <button type="button" className="btn btn-light" onClick={signOut}>Sign out</button>
+              : <div className="col-lg-12 mx-auto">
+                  <button type="button" className="btn btn-danger" onClick={signInWithGoogle}>Google</button>
+                  
+                </div>  
+          }
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCslTup0Yo3J1704r9Ktt_uN0fZfVEivQ4",
+    authDomain: "lk-react-authentication.firebaseapp.com",
+    databaseURL: "https://lk-react-authentication.firebaseio.com",
+    projectId: "lk-react-authentication",
+    storageBucket: "lk-react-authentication.appspot.com",
+    messagingSenderId: "623333908634"
+};
